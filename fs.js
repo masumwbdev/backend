edff3cf2f49f3a238ea0efba3a494bd79824ba8f
masumwdv <masumwdv@gmail.com>
2022-08-24 12:29:20 +0600

    asyncronus way fs

--- fs.js
+++ fs.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 // fs.writeFileSync('appendFs.txt', 'Hello world ');
 // fs.appendFileSync('appendFs.txt', 'Appending file system');
 
-const data = fs.readFileSync('writeFs.txt');
+// syncronus way
+// const data = fs.readFileSync('writeFs.txt');
 // console.log(data);
-console.log(data.toString());
+// console.log(data.toString());
+
+fs.readFile('writeFs.txt', (err, data) => {
+    console.log(data.toString());
+});
+console.log("hello man");
